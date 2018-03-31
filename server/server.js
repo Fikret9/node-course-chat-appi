cbe9fabe978dabac40672a84342d905646a546ad
fikret <f_tokyay@yahoo.com>
2018-03-30 20:55:09 -0500

    socket io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname,'../public');
+const socketIO = require('socket.io');
 const port = process.env.PORT || 3000;
 
-express = require('express');
-app = express();
+var express = require('express');
+var app = express();
+var server = http.createServer(app) ;
+var io = socketIO(server);
+io.on('connection',(socket) =>{
+	console.log('New user connected');
+	socket.on('disconnect',(socket) =>{
+	console.log('Client disconnect !');
+	});
+});
+
+
+
+
 
 app.use(express.static(publicPath));
 
 
-app.listen(port,() => {
+server.listen(port,() => {
   console.log(`Server is up on port ${port}`);
 });
