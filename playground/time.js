c486744cc96c2db2393eb7e968549fd4ac2e11e6
fikret <f_tokyay@yahoo.com>
2018-04-01 20:52:12 -0500

    add

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+// jan 1st 1970 0 0 0 am
+const moment = require('moment');
+// var date = new Date();
+// console.log(date.getMonth());
+
+ var  date = moment();
+
+ console.log(date.format('h:mm a'));
+
+//10:35 am
+
+
