62cba4ffbdf6b539bf44dd4cafc0369a59fa4a6e
fikret <f_tokyay@yahoo.com>
2018-04-02 20:37:07 -0500

    add join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,19 @@
  
 	var socket = io();
+
+	function scrollToBottom(){
+		var messages = jQuery('#messages');
+		var  newMessage =  messages.children('li:last-child');
+		var clientHeight = messages.prop('clientHeight');
+		var scrollTop = messages.prop('scrollTop');
+		var scrollHeight = messages.prop('scrollHeight');
+		var newMessageHeight = newMessage.innerHeight();
+		var lastMessageHeight = newMessage.prev().innerHeight();
+		if (clientHeight + scrollTop  + newMessageHeight + lastMessageHeight>= scrollHeight){ 
+			 messages.scrollTop(scrollHeight);
+		}
+	}
+
 	socket.on('connect', function () {
 		console.log('connected to server');  
 
@@ -22,6 +36,7 @@
 		  });
 
 		  jQuery('#messages').append(html);
+		  scrollToBottom();
 		  
 	});
 
@@ -36,6 +51,7 @@
 		  });
  
 		jQuery('#messages').append(html);
+		scrollToBottom();
 	});
 
 
